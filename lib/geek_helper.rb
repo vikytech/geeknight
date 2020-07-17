8756b9acf1b5d393aa11660ec5b59517ab9ed8a8
Vikhyath <vikhyatc@thoughtworks.com>
2020-07-17 10:13:34 +0530

    Viky | Removed JS and updated CSS

--- lib/geek_helper.rb
+++ lib/geek_helper.rb
@@ -20,23 +20,7 @@ module GeekHelper
     return @items.select {|event| !event[:start].nil?}.sort_by { |i| i[:edition]}.reverse.group_by {|i| [i[:start].to_date.year, i[:start].to_date.strftime("%b %d")]}
   end
 
-  def getEvents
-    events=[]
-    groupYearMonths.each do |event|
-      events.push({event[0].first=>event[0].last})
-    end
-    return Hash[events.group_by {|i| i.keys}.map{|k,v| [k, v.map{ |k1|  k1.map{|k2,v2| v2 }}.flatten] }]
-  end
-
   def latest
     @@first ||= archives.first
-  end
-
-  def json_of_event(item)
-    {'@context' => 'http://schema.org', '@type' => 'EducationEvent', 'name' => "Geek Night Chennai - #{Time.parse(item[:start]).strftime("%b %Y")}",
-     'startDate' => Time.parse(item[:start]).iso8601, 'url' => "http://twchennai.github.io/geeknight#{item.path}",
-     'location' => {'@type' => 'Place', 'name' => 'ThoughtWorks', 'address' => {
-        '@type' => 'PostalAddress', 'addressLocality' => 'Chennai', 'addressRegion' => 'Tamil Nadu', 'postalCode' => '600113', 'streetAddress' => 'Ascendas'
-    }}}.to_json
   end
 end
